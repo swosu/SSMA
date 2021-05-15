4ef5f0f0d4bb19eabeb43ce4dbfb0bf4318be0e0
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-05-15 13:54:00 -0500

    added a random number generator.

--- CoinToss/FirstProgram/CoinToss.py
+++ CoinToss/FirstProgram/CoinToss.py
@@ -1,4 +1,8 @@
+import random
+
 print("Hello, and welcome to our coin toss program.")
 
 # Toss a single coin and see if it comes up heads or tails.
 
+our_number = random.random()
+print("Our first number is: ",our_number)
