046fa69cc7b7a6edba0efa0d727c69e32116d662
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-05-15 20:50:46 -0500

    able to do a set of batches of coin tosses.

---
+++ CoinToss/ThirdProgram/test_batch_coin_system.py
@@ -0,0 +1,18 @@
+import batch_coin_toss
+
+batch_size = 2
+repeat_count = 10
+balanced_head_count = batch_size / 2
+balanced_count = 0
+
+for i in range(repeat_count):
+    head_count = batch_coin_toss.batch_toss_coins(batch_size)
+    if(head_count == balanced_head_count):
+        balanced_count = balanced_count + 1
+
+
+percentage_balanced_sets = balanced_count/repeat_count
+print('Our balanced set count was', balanced_count, 
+'out of', repeat_count, 'total batches')
+print(f'We flipped a balanecd set, 
+{percentage_balanced_sets: 0.2f}% of the time')
