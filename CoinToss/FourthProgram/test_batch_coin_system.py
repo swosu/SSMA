d8dc530137be05fa0380519881be02d44dbb54c3
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-05-17 08:57:23 -0500

    able to loop through larger batches.

--- CoinToss/FourthProgram/test_batch_coin_system.py
+++ CoinToss/FourthProgram/test_batch_coin_system.py
@@ -1,20 +1,22 @@
 import batch_coin_toss
 import time
 
-batch_size = 12
-repeat_count = 1000000
-balanced_head_count = batch_size / 2
-balanced_count = 0
+for batch_size in range(2,14,2):
+    repeat_count = 1000000
+    balanced_head_count = batch_size / 2
+    balanced_count = 0
 
-start_time = time.time()
-for i in range(repeat_count):
-    head_count = batch_coin_toss.batch_toss_coins(batch_size)
-    if(head_count == balanced_head_count):
-        balanced_count = balanced_count + 1
+    start_time = time.time()
+    for i in range(repeat_count):
+        head_count = batch_coin_toss.batch_toss_coins(batch_size)
+        if(head_count == balanced_head_count):
+            balanced_count = balanced_count + 1
 
-end_time = time.time()
-elapsed_time = end_time - start_time
-percentage_balanced_sets = balanced_count/repeat_count
-print('Our balanced set count was', balanced_count, 
-'out of', repeat_count, 'total batches')
-print(f'We flipped a balanecd set, {100*percentage_balanced_sets: 0.2f}% of the time in {elapsed_time: f} seconds.')
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    percentage_balanced_sets = balanced_count/repeat_count
+    print(f'batch size, {batch_size: d}',
+    f' percentage {100*percentage_balanced_sets: 0.2f}%'
+    f' time in {elapsed_time: f} seconds.')
+
+print('End of the program. Take care!')
